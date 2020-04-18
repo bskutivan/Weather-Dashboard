b1eaef8aa31a987292adac6c8e996103c0c386ca
BrandonKutivan <bskutivan@gmail.com>
2020-04-18 16:16:14 -0700

    created function to display current weather data and function to get current UV information.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,24 @@
 
 // city search handler function
     // captures input value and feeds into search city function
+
 var searchFormEl = document.getElementById("searchForm");
 var searchInputEl = document.getElementById("search");
+var cityNameEl = document.getElementById("cityName");
 
-var displayCurrentWeather = function() {
-    
+
+var displayCurrentWeather = function(data) {
+    console.log("Weather!");
+    var lon = data.coord.lon;
+    var lat = data.coord.lat;
+    var name= data.name
+
+    searchCityUV(lon, lat, name);
 }
+
+// var displayCurrentUV = function() {
+//     console.log("sunburns")
+// }
 
 var searchSubmitHandler = function(event) {
     event.preventDefault();
@@ -22,11 +34,28 @@ var searchSubmitHandler = function(event) {
 
     if(city) {
         searchCityCurrent(city);
-
         searchInputEl.value = "";
     } else {
         alert("Please enture the name of the city you want the forecast for.");
     }
+}
+
+var searchCityUV = function(name, lon, lat) {
+    console.log(lon);
+    console.log(lat);
+    console.log(name);
+
+    // var apiUrl = "http://api.openweathermap.org/data/2.5/uvi?q=" + city + "&APPID=e51cf34ee1831280d9d0aec1b510446b&lat=" + lat + "&lon=" +lon;
+
+    // fetch(apiUrl).then(function(response) {
+    //     if (response.ok) {
+    //         response.json().then(function(data) {
+    //             displayCurrentUV(data);
+    //         });
+    //     } else {
+    //         alert("Error: " + response.statusText);
+    //     }
+    // })
 }
 
 var searchCityCurrent = function(city) {
@@ -35,7 +64,7 @@ var searchCityCurrent = function(city) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                displayCurrentWeather(data);
+                displayCurrentWeather(data, city);
             });
         } else {
             alert("Error: " + response.statusText);
