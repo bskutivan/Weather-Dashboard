afee4752c198a9aa8d359442a1d023f5593a126b
BrandonKutivan <bskutivan@gmail.com>
2020-04-18 15:37:23 -0700

    adjusted API request for search input and adjusted for imperial units

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,40 @@
 
 // city search handler function
     // captures input value and feeds into search city function
-var searchCity = function() {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=e51cf34ee1831280d9d0aec1b510446b"
+var searchFormEl = document.getElementById("searchForm");
+var searchInputEl = document.getElementById("search");
+
+var displayCurrentWeather = function() {
+    
+}
+
+var searchSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    var city = searchInputEl.value.trim();
+
+    if(city) {
+        searchCityCurrent(city);
+
+        searchInputEl.value = "";
+    } else {
+        alert("Please enture the name of the city you want the forecast for.");
+    }
+}
+
+var searchCityCurrent = function(city) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=e51cf34ee1831280d9d0aec1b510446b"
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
-            })
+                displayCurrentWeather(data);
+            });
         } else {
             alert("Error: " + response.statusText);
         }
-    })
-
+})
 
 }
 
+searchFormEl.addEventListener("submit", searchSubmitHandler)
