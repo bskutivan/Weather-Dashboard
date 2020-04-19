72e084856956599b3d066ce9ebbd362bb7c768da
BrandonKutivan <bskutivan@gmail.com>
2020-04-18 21:50:11 -0700

    added values for each HTML element to have temp, humidiy, winde speed, and UV index information added. Captured these values from both api fetch requests and used conditional statements to adjust UV index background colors with bootstrap classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,43 @@
-// search for city
-    // fetch request for city info
-    // capture data 
-    // feed data to new function
-
-// display city
-    // take fed data and display city info and adjust card info
-
-// city search handler function
-    // captures input value and feeds into search city function
-
+    //current conditions variables and search form
 var searchFormEl = document.getElementById("searchForm");
 var searchInputEl = document.getElementById("search");
 var cityNameEl = document.getElementById("cityNameText");
 var iconEl = document.getElementById("icon");
+var currentTempEl = document.getElementById("currentTemp");
+var currentHumidityEl = document.getElementById("currentHumidity")
+var currentWindSpeedEl = document.getElementById("currentWindSpeed")
+var currentUVEl = document.getElementById("currentUV")
+
 
 var displayCurrentWeather = function(data) {
     //variables defined to retain data points needed to pass into UV fetch request and populate current weather section
     var lon = data.coord.lon;
     var lat = data.coord.lat;
+    //get city name
     var city = data.name;
-    //var temp = data.main.temp
+    var cityTitleEl = document.createElement("span")
+    cityTitleEl.innerHTML = city
+
+    //get temp
+    var temp = data.main.temp;
+    currentTempEl.innerHTML = " " + temp + "°F"
+
+    //get humidity
+    var humidity = data.main.humidity;
+    currentHumidityEl.innerHTML = " " + humidity + "%"
+
+    //get wind speed
+    var windSpd = data.wind.speed;
+    currentWindSpeedEl.innerHTML = " " + windSpd + "MPH"
+
     
+    //get icon code
     var iconCode = data.weather[0].icon;
-    console.log(iconCode);
     iconEl.setAttribute("src","http://openweathermap.org/img/wn/" + iconCode +"@2x.png");
-    console.log(iconEl);
+    //get date code
     var date = moment.unix(data.dt).format("L")
     var dateEl = document.createElement("span")
-    dateEl.innerHTML = "(" + date + ")"
-    
+    dateEl.innerHTML = " (" + date + ")"
     
     var cityTitleEl = document.createElement("span")
     cityTitleEl.innerHTML = city
@@ -38,13 +47,22 @@ var displayCurrentWeather = function(data) {
     cityNameEl.appendChild(cityTitleEl);
     cityNameEl.appendChild(dateEl);
     
-
-    console.log(date)
     searchCityUV(lon, lat, city);
 }
 
-var displayCurrentUV = function() {
-    console.log("sunburns")
+var displayCurrentUV = function(data) {
+    var uv = data.value;
+    console.log(uv)
+    if (uv >= 6) {
+        currentUVEl.classList="bg-danger m-3 p-2 border rounded text-center";
+        currentUVEl.innerHTML=" " + uv + " ";
+    } else if (uv > 3) {
+        currentUVEl.classList="bg-warning m-3 p-2 border rounded text-center";
+        currentUVEl.innerHTML=" " + uv + " ";
+    } else {
+    currentUVEl.classList="bg-primary m-3 p-2 border rounded text-center";
+    currentUVEl.innerHTML=" " + uv + " ";
+    }
 }
 
 var searchSubmitHandler = function(event) {
